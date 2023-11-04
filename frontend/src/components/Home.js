18d22937d2d315465b7d733fe8b1b6595c978142
Amanda McGilchrist <z5358641@unsw.edu.au>
2023-11-04 23:23:05 +1100

    redesigned Home Screen

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,33 +7,32 @@ import Examples from "./Examples";
 
 function Home () {
   return(<>
-  <ThemeProvider theme={theme}>
-    <Box width='100%'>
-      <Box sx={{ height: '150px', backgroundColor: 'primary.main', borderRadius: '0 0 15px 15px', width: '100%', justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
-        <Typography variant="h4" color='white'>Asymmetric Cryptography</Typography>
-      </Box>
-
-      <Box width='90%' m='auto' mt={2}>
-        <Box mt='15px' mb='15px' p='10px'>
-          <Typography>
-            Asymmetric cryptography is a method of encryption where two different keys
-            are used: a public key and a private key. The public key is used for 
-            encryption, while the private key is used for decryption. Since the keys 
-            are different, even if someone has the public key, they cannot decrypt a 
-            message without the corresponding private key.
+  <ThemeProvider theme={theme} >
+    <Box width='100%' height='720px' display='flex' justifyContent='center' mt={10}>
+      <Grid container direction='row' alignItems='center' height='100%' width='80%' justifyContent='center' spacing={4}>
+        <Grid item xs={12} md={6} lg={5} mr={{ md: 10 }}>
+          <Typography 
+            fontSize={35} 
+            color='primary.main' 
+            mb={2} 
+            borderBottom='2px solid'
+            textAlign={{ sm: 'center', md: 'left', lg: 'center'}}
+          >
+            Asymmetric Cryptography
           </Typography>
-        </Box>
-
-        <Typography variant="h5">Terminology</Typography>
-          <Terminology/>
-        <Typography variant="h5">Uses</Typography>
-        <Uses/>
-        <Typography variant="h5">Examples</Typography>
-          <Examples/>
-      </Box>
-      
-
-
+          <Typography textAlign="justify">
+            Asymmetric cryptography is a method of encryption where two 
+            different keys are used: a public key and a private key. The
+            public key is used for encryption, while the private key is 
+            used for decryption. Since the keys are different, even if someone
+            has the public key, they cannot decrypt a message without the 
+            corresponding private key.
+          </Typography>
+        </Grid>
+        <Grid item xs md lg>
+          <Uses/>
+        </Grid>
+      </Grid>
     </Box>
   </ThemeProvider>
   </>)
